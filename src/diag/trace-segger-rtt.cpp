1f5899e6ef289858786174de8584d62975887071
Liviu Ionescu <ilg@livius.net>
2025-08-23 22:56:30 +0300

    trace-segger-rtt.cpp cast & cosmetics

--- src/diag/trace-segger-rtt.cpp
+++ src/diag/trace-segger-rtt.cpp
@@ -1,16 +1,13 @@
 /*
- * This file is part of the µOS++ distribution.
- *   (https://github.com/micro-os-plus/)
- * Copyright (c) 2016 Liviu Ionescu.
+ * This file is part of the µOS++ project (https://micro-os-plus.github.io/).
+ * Copyright (c) 2016-2025 Liviu Ionescu.
  *
  * Permission to use, copy, modify, and/or distribute this software
  * for any purpose is hereby granted, under the terms of the MIT license.
  *
  * If a copy of the license was not distributed with this file, it can
- * be obtained from https://opensource.org/licenses/MIT/.
+ * be obtained from https://opensource.org/licenses/mit.
  */
-
-#if defined(__ARM_EABI__)
 
 // ----------------------------------------------------------------------------
 
@@ -44,11 +41,28 @@ namespace micro_os_plus
     {
       SEGGER_RTT_Init ();
 
+#if defined(__ARM_EABI__)
+
       // Clear the SLEEPDEEP.
       // This does not guarantee that the WFI will not prevent
       // the J-Link to read the RTT buffer, but it is the best it
       // can be done at this level.
+      // TODO: document where this comes from.
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#if defined(__clang__)
+#else
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#endif
+#endif
       SCB->SCR &= ~SCB_SCR_SLEEPDEEP_Msk;
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+
+#else
+#warning "Architecture not supported"
+#endif // defined(__ARM_EABI__)
     }
 
     // ------------------------------------------------------------------------
@@ -68,8 +82,12 @@ namespace micro_os_plus
 
       ssize_t ret;
 
+      // SEGGER provides a `SEGGER_RTT_Write()` which implements locking
+      // with its own `SEGGER_RTT_MAX_INTERRUPT_PRIORITY`.
+      // For consistency with µOS++ interrupt handling, a µOS++ critical
+      // section is preferred.
       rtos::interrupts::critical_section ics;
-      ret = (ssize_t)SEGGER_RTT_WriteNoLock (0, buf, nbyte);
+      ret = static_cast<ssize_t> (SEGGER_RTT_WriteNoLock (0, buf, nbyte));
 
       return ret;
     }
@@ -77,6 +95,8 @@ namespace micro_os_plus
     void
     flush (void)
     {
+      // Busy wait until all data is sent.
+      // The buffer is empty when WrOff == RdOff.
       while (_SEGGER_RTT.aUp[0].WrOff != _SEGGER_RTT.aUp[0].RdOff)
         {
           __NOP ();
@@ -89,9 +109,5 @@ namespace micro_os_plus
 
 #endif // defined(MICRO_OS_PLUS_USE_TRACE_SEGGER_RTT)
 #endif // defined(MICRO_OS_PLUS_TRACE)
-
-// ----------------------------------------------------------------------------
-
-#endif // defined(__ARM_EABI__)
 
 // ----------------------------------------------------------------------------
